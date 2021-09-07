0791a4a22ab7311698d9baf0c0a24886e2696c88
reydaniels39 <reydaniels@outlook.com>
2021-09-07 15:12:14 -0500

    Agregado el Cifrado al Cliente

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -1,11 +1,36 @@
 import socket
+from cryptography.fernet import Fernet
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 1002)) #192.168.1.252
+client.connect(('localhost', 1002))
 
-print('Enviando')
-file = open('imagen_ori.jpg', 'rb')
 #AQUI VA EL CIFRADO SIMETRICO
+
+#Crear y guardar la llave
+#key = Fernet.generate_key()
+#print('Llave:',key)
+#with open('llave.key','wb') as archivo_key:
+    #archivo_key.write(key)
+    #archivo_key.close()
+
+#Leer la clave
+llave = open('llave.key','rb')
+
+#Metodo Fernet
+f = Fernet(llave)
+
+#Encriptar archivo
+file = open('imgOriginal.jpg', 'rb')
+archivo = file.read()
+encrypted_file = f.encrypt(archivo)
+file.close()
+file = open('imgOriginal.jpg', 'wb')
+file.write(encrypted_file)
+file.close()
+
+#Aquí enviamos la imagen encriptada
+print('Enviando')
+file = open('imgOriginal.jpg', 'rb')
 image_data = file.read(2048)
 
 while image_data:
