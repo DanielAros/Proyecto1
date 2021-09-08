72e407785b75978b0091372d4c4373a82df39d92
Daniel Aros <danielaros200@gmail.com>
2021-09-07 20:36:33 -0500

    Se agrego la función de comparar imagenes al Cliente

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -1,5 +1,8 @@
 import socket
 from cryptography.fernet import Fernet
+import cv2 #Es la libreria de OpenCV. Se debe instalar con el comando "pip install opencv-contrib-python"
+import numpy as np
+import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 1002))
@@ -64,3 +67,27 @@ print('Exito')
 
 #AQUÍ VA LA COMPARACIÓN
 #---------------------------------------------
+os.chdir('./Cliente')#Se posiciona en el directorio donde se encuentran las imagenes
+
+#Cargamos las imagenes a comparar, se pasa el nombre del archivo y el 1 es para leer la imagen a color
+imagen1 = cv2.imread("imgOriginal.jpg",1)
+imagen2 = cv2.imread("imagen_comparar.jpg.jpg",1)#Imagen descifrada
+
+def comparar(img1,img2):
+    diferencia = cv2.subtract(img1,img2)#Lee los arreglos de pixeles de cada imagen y los compara haciendo una resta.
+    if not np.any(diferencia):
+        print("Las imagenes son iguales")
+        #Mostar las dos imagenes
+        imas = np.hstack((imagen1,imagen2))#Toma las imagenes a visualizar de forma conjunta
+        cv2.imshow("Photo", imas)
+        cv2.waitKey(0)
+    else:
+        cv2.imwrite("img_diferencia.jpg",diferencia) #Se crea una imagen con las diferencias encontradas
+        imagenDiferente = cv2.imread('img_diferencia.jpg')
+        print("Las imagenes son distintas")
+        #Mostar las imagenes
+        ima = np.hstack((imagen1, imagenDiferente, imagen2))
+        cv2.imshow("Photo", ima)
+        cv2.waitKey(0)
+
+comparar(imagen1,imagen2)
